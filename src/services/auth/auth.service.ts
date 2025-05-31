0cc25ea87f82155381f179ed1f99f42409ef6bc8
David <david@Davo-PC.(none)>
2025-05-31 01:26:45 -0600

    feat: registo de usuarios con validaciones

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,18 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaDbService } from '../prisma-db/prisma-db.service';
+
+import * as bcrypt from 'bcrypt';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
+
+interface RegistroInput {
+  email: string;
+  password: string;
+  name: string;
+}
 
 @Injectable()
 export class AuthService {
   constructor(private db: PrismaDbService) {}
 
-  register(registerDto: any): string {
-    console.log('Register DTO:', registerDto);
-    // Aquí podrías agregar validaciones o lógica adicional
-    // Lógica para el registro
-    return 'This action adds a new user';
-    this.db.user.create({
-      data: {},
-    });
+  async register(
+    registroData: RegistroInput,
+    ip?: string,
+    userAgent?: string,
+  ): Promise<any> {
+    const hashedPassword = await bcrypt.hash(registroData.password, 107);
+
+    try {
+      const newUser = await this.db.user.create({
+        data: {
+          email: registroData.email,
+          passwordHash: hashedPassword,
+          name: registroData.name,
+          auditLogs: {
+            create: {
+              action: 'register',
+              ipAddress: ip,
+              userAgent: userAgent,
+            },
+          },
+        },
+        include: {
+          auditLogs: true,
+        },
+        omit: {
+          passwordHash: true,
+        },
+      });
+      return newUser;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException(
+          'Email already exists',
+          JSON.stringify(error),
+        );
+      } else if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new UnprocessableEntityException(
+          'Invalid data',
+          JSON.stringify(error),
+        );
+      } else {
+        throw new InternalServerErrorException(
+          'Error creating user',
+          JSON.stringify(error),
+        );
+      }
+    }
   }
 
   login(req: any): string {
